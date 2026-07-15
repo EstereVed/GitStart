3203e6d1de9813a0b7919e8ddb98b083e6776338
EstereVed <e.vedrickaite@ba.lt>
2026-07-15 15:39:07 +0300

    add test to add all items to cart (#4)
    new test to add all items to cart and check item count in the cart added

--- tests/pirmas.spec.js
+++ tests/pirmas.spec.js
@@ -39,5 +39,18 @@ test('patikrinti teksta', async ({ page }) => {
 });
 
 //2026-07-14:
-//pasirašyti for, kad įdėtų visas prekes į krepšelį
-//pasidaryti branchą, sudėti visus test failus į git'ą ir  for loopą
+//pasidaryti branchą, sudėti visus test failus į git'ą 
+//pasirašyti for, kad įdėtų visas prekes į krepšelį ir patikrinti, kad įdėtos 6 prekės
+test('add all items to cart', async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+    const cartPage = new CartPage (page);
+    const cartBadge = page.locator ('.shopping_cart_badge');
+        
+    loginPage.userLoginWithPassword('standard_user','secret_sauce');
+    cartPage.addAllItemsToCart();
+    
+    await expect(cartBadge).toHaveText("6");
+
+    
+});
