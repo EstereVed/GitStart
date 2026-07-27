35fe6a9233db1df05e82fc7a053642ac2b501d7b
Estere V <e.vedrickaite@ba.lt>
2026-07-27 14:48:20 +0300

    new test to add all items to cart
    new test to add all items to cart added, previous test renamed to addSixItemsToCart

--- tests/pirmas.spec.js
+++ tests/pirmas.spec.js
@@ -11,13 +11,12 @@ test('pirmas testas', async ({ page }) => {
     const cartPage = new CartPage (page);
     const checkoutPage =new CheckoutPage (page);
     
-    loginPage.userLoginWithPassword('standard_user','secret_sauce');
-    cartPage.addItemToCart();
-    checkoutPage.checkoutFirstStep('Vardenis','Pavardenis','12345');
-    checkoutPage.finishCheckout();
+    await loginPage.userLoginWithPassword('standard_user','secret_sauce');
+    await cartPage.addItemToCart();
+    await checkoutPage.checkoutFirstStep('Vardenis','Pavardenis','12345');
+    await checkoutPage.finishCheckout();
 
-    await page.pause();
-
+    
     
 });
 
@@ -32,7 +31,7 @@ test('patikrinti teksta', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const backPack = page.locator ('[data-test="inventory-item-name"]').first();
     
-    loginPage.userLoginWithPassword('standard_user','secret_sauce');
+    await loginPage.userLoginWithPassword('standard_user','secret_sauce');
     await expect(backPack).toHaveText("Sauce Labs Backpack");
 
     
@@ -41,16 +40,37 @@ test('patikrinti teksta', async ({ page }) => {
 //2026-07-14:
 //pasidaryti branchą, sudėti visus test failus į git'ą 
 //pasirašyti for, kad įdėtų visas prekes į krepšelį ir patikrinti, kad įdėtos 6 prekės
-test('add all items to cart', async ({ page }) => {
+test('add 6 items to cart', async ({ page }) => {
     
     const loginPage = new LoginPage(page);
     const cartPage = new CartPage (page);
     const cartBadge = page.locator ('.shopping_cart_badge');
         
-    loginPage.userLoginWithPassword('standard_user','secret_sauce');
-    cartPage.addAllItemsToCart();
+    await loginPage.userLoginWithPassword('standard_user','secret_sauce');
+    await cartPage.addSixItemsToCart();
     
     await expect(cartBadge).toHaveText("6");
 
     
+});
+
+//2026-07-16:
+//susirasti kiek itemų ir pasirašyti for, kad apskaičiuotų neįhardkodintą skaičių itemų
+//iškviesti metodą kuri paduodamas css selector, kuris bus naudojamas kaip parametras
+test('add all items to cart', async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+    const cartPage = new CartPage (page);
+    //const addToCartButton = page.locator('.btn.btn_primary.btn_small.btn_inventory').first();
+    const cartBadge = page.locator ('.shopping_cart_badge');
+    //var itemCount = page.locator('.btn.btn_primary.btn_small.btn_inventory').count();
+    
+    await loginPage.userLoginWithPassword('standard_user','secret_sauce');
+    await cartPage.addAllItemsToCart('.btn.btn_primary.btn_small.btn_inventory');
+    /*for(let i=1; i<=itemCount; i++){
+       await this.addToCartButton.click(); 
+    }*/
+    await expect(cartBadge).toHaveText("6")
+    
+    await page.pause();      
 });
