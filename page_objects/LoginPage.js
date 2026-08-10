3a28323ecf9eca17256315a3ccd6cc7f44b2e7f9
Estere V <e.vedrickaite@ba.lt>
2026-08-10 11:00:57 +0300

    add new login test
    new test to try to login with invalid credentials added

--- page_objects/LoginPage.js
+++ page_objects/LoginPage.js
@@ -1,18 +1,26 @@
 export class LoginPage{
 
-constructor(page){
-    this.page = page;
-    //this.username = page.locator('#user-name');
-    //this.password = page.locator('#password');
-    //this.loginButton = page.locator('#login-button');
-
-    //Xpath:
-    this.username = page.locator('xpath =//*[@id="user-name"]');
-    //this.password = page.locator('xpath =//div[@class ="input_error form_input"][last()]');
-    this.password = page.locator('xpath =//*[@id ="password"]');
-    //this.loginButton = page.locator('xpath = //*[@id ="login-button"]');
-    this.loginButton = page.locator('xpath = //*[@name ="login-button"]');
+    constructor(page){
+    this.page = page; 
+    this.signupLoginButton = page.locator('a[href="/login"]');
+    this.username = page.locator('[data-qa="login-email"]');
+    this.password = page.locator('[data-qa="login-password"]');
+    this.loginButton = page.locator('[data-qa="login-button"]');
 }
+
+// constructor(page){
+//     this.page = page;
+//     //this.username = page.locator('#user-name');
+//     //this.password = page.locator('#password');
+//     //this.loginButton = page.locator('#login-button');
+
+//     //Xpath:
+//     this.username = page.locator('xpath =//*[@id="user-name"]');
+//     //this.password = page.locator('xpath =//div[@class ="input_error form_input"][last()]');
+//     this.password = page.locator('xpath =//*[@id ="password"]');
+//     //this.loginButton = page.locator('xpath = //*[@id ="login-button"]');
+//     this.loginButton = page.locator('xpath = //*[@name ="login-button"]');
+// }
 
 //bdd šitos dalies nereikia:
 
